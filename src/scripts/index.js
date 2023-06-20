0d2ee8a3fe896bd190693aaed806c40067cdbe6e
Lucas Rocha Gonçalves <86074220+lrglucas97@users.noreply.github.com>
2023-06-20 12:28:25 -0400

    removing console.log for debugging

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -36,7 +36,6 @@ async function getUserData(userName) {
   user.setInfo(userResponse);
   user.setRepositories(repositoriesResponse);
   user.setEvents(eventsResponse)
-  console.log(eventsResponse)
 
   screen.renderUser(user);
 }
